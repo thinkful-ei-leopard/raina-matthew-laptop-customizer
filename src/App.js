a8eb1525c96a5b3970a825727e5dbfc1c9c6a413
Raina Shin <yewon95@vt.edu>
2020-01-31 11:45:14 -0500

    finished refactoring

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import './App.css';
 
 import Header from './components/Header'
 import Customize from './components/Customize'
+import Summary from './components/Summary'
 
 // This object will allow us to
 // easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
+
 
 class App extends Component {
   state = {
@@ -43,41 +41,14 @@ class App extends Component {
   };
 
   render() {
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
+    
 
     return (
       <div className="App">
         <Header />
         <main>
           <Customize features={this.props.features} selected={this.state.selected} handleChange={(feature, newValue) => this.updateFeature(feature, newValue)} />
-          <section className="main__summary">
-            <h2>Your cart</h2>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
-          </section>
+          <Summary selected={this.state.selected}/>
         </main>
       </div>
     );
---
+++ src/components/Total.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Total(props) {
+    const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      });
+
+    const total = Object.keys(props.selected).reduce(
+        (acc, curr) => acc + props.selected[curr].cost,
+        0
+      );
+
+    return (
+        <div className="summary__total">
+              <div className="summary__total__label">Total</div>
+              <div className="summary__total__value">
+                {USCurrencyFormat.format(total)}
+              </div>
+         </div>
+    )   
+
+}
+
+export default Total
